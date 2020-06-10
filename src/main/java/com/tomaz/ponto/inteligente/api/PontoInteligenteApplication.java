debb6ebd18154fb58985d9a71cce6f101f017bfb
marcelo_tomazzini <marcelo_tomazzini@terceiros.sicredi.com.br>
2020-06-09 21:51:32 -0300

    desabilitado sequence do spring

--- src/main/java/com/tomaz/ponto/inteligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/tomaz/ponto/inteligente/api/PontoInteligenteApplication.java
@@ -2,12 +2,13 @@ package com.tomaz.ponto.inteligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PontoInteligenteApplication.class, args);
+		SpringApplication.run(PontoInteligenteApplication.class, args );
 	}
 
 }
--- src/main/java/com/tomaz/ponto/inteligente/api/controller/CadastroPJController.java
+++ src/main/java/com/tomaz/ponto/inteligente/api/controller/CadastroPJController.java
@@ -1,19 +1,31 @@
 package com.tomaz.ponto.inteligente.api.controller;
 
+import java.security.NoSuchAlgorithmException;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tomaz.ponto.inteligente.api.dto.CadastroPJDto;
+import com.tomaz.ponto.inteligente.api.entities.Empresa;
+import com.tomaz.ponto.inteligente.api.entities.Funcionario;
+import com.tomaz.ponto.inteligente.api.enums.PerfilEnum;
 import com.tomaz.ponto.inteligente.api.response.Response;
 import com.tomaz.ponto.inteligente.api.service.EmpresaService;
 import com.tomaz.ponto.inteligente.api.service.FuncionarioService;
+import com.tomaz.ponto.inteligente.api.util.PasswordUtils;
 
+@RestController
+@RequestMapping("/api/cadastrar-pj")
+@CrossOrigin(origins = "*")
 public class CadastroPJController {
 
 	@Autowired
@@ -21,6 +33,8 @@ public class CadastroPJController {
 	
 	@Autowired
 	private EmpresaService empresaService;
+	
+	private CadastroPJDto cadastroPJDto;
 	
 	public CadastroPJController() {
 	}
@@ -32,7 +46,54 @@ public class CadastroPJController {
 		Response<CadastroPJDto> response = new Response<CadastroPJDto>();
 		
 		validarDadosExistentes(cadastroPJDto, result);
-		return null;
+		var empresa = this.converterDtoParaEmpresa(cadastroPJDto);
+		var funcionario = this.converterDtoParaFuncionario(cadastroPJDto, result);
+		
+		if(result.hasErrors()) {
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		this.empresaService.persistir(empresa);
+		funcionario.setEmpresa(empresa);
+		this.funcionarioService.persistir(funcionario);
+		
+		response.setData(this.converterCadastroPJDto(funcionario));
+		return ResponseEntity.ok(response);
+	}
+	
+	private Empresa converterDtoParaEmpresa(CadastroPJDto cadastroPJDto) {
+		var empresa = new Empresa();
+		
+		empresa.setCnpj(cadastroPJDto.getCnpj());
+		empresa.setRazaoSocia(cadastroPJDto.getRazaoSocial());
+		
+		return empresa;
+	}
+	
+	private Funcionario converterDtoParaFuncionario(CadastroPJDto cadastroPJDto, BindingResult result) {
+		var funcionario = new Funcionario();
+		
+		funcionario.setNome(cadastroPJDto.getNome());
+		funcionario.setEmail(cadastroPJDto.getEmail());
+		funcionario.setCpf(cadastroPJDto.getCpf());
+		funcionario.setPerfil(PerfilEnum.ROLE_ADMIN);
+		funcionario.setSenha(PasswordUtils.gerarBCrypt(cadastroPJDto.getSenha()));
+		
+		return funcionario;
+	}
+	
+	private CadastroPJDto converterCadastroPJDto(Funcionario funcionario) {
+		var cadastroPJDto = new CadastroPJDto();
+		
+		cadastroPJDto.setId(funcionario.getId());
+		cadastroPJDto.setNome(funcionario.getNome());
+		cadastroPJDto.setEmail(funcionario.getEmail());
+		cadastroPJDto.setCpf(funcionario.getCpf());
+		cadastroPJDto.setRazaoSocial(funcionario.getEmpresa().getRazaoSocia());
+		cadastroPJDto.setCnpj(funcionario.getEmpresa().getRazaoSocia());
+		
+		return cadastroPJDto;
 	}
 	
 	private void validarDadosExistentes(CadastroPJDto cadastroPJDto, BindingResult result) {
